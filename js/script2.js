c174a3726f38fa5b31533ec6a25d383166529a6e
Tina Pollard <smithtina1203@aol.com>
2019-09-18 15:10:27 -0500

    Hands-On: Using JavaScript Data Types

---
+++ js/script2.js
@@ -0,0 +1,7 @@
+//Hands-On: Using JavaScript Data Types
+var $elem = document.getElementById('output');    
+$elem.innerHTML += 'Test';
+$elem.innerHTML += 4 + 6;
+$elem.innerHTML += 4 + 6 + ' Demo';
+$elem.innerHTML += 'Demo ' + 4 + 6;
+$elem.innerHTML += 'Demo ' + (4 + 6);
